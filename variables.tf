bce94ff1da18a59058136b3aaa2d1ed5f16fdc6f
sankarvg.devops <sankarvg.devops@gmail.com>
2023-06-15 17:24:29 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -10,11 +10,12 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
+  default     = "sankar"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "uksouth"
 }
 
 variable "address_space" {
